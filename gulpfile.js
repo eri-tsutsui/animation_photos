1a25da47a034148b223773701e30c43baf29dede
Eri <izutsu812@gmail.com>
2020-03-30 14:58:02 +0900

    アニメーションの練習+Gulpの練習（sassのコンパイル+画像の圧縮）※watchが動かない..

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// 画像の圧縮
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+
+gulp.task("imagemin", () =>
+  gulp
+    .src("images/img01.jpg")
+    .pipe(imagemin()) // 画像の最適化処理
+    .pipe(gulp.dest("dist")) // 最適化済みの画像を書き出すフォルダー
+);
+
+
+
+// sassのコンパイル
+const sass = require("gulp-sass");
+
+gulp.task("sass", () =>
+  gulp
+    .src("scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("scss")) // cssを書き出すディレクトリの指定
+);
+
+
+
+// taskの登録
+gulp.task("default", gulp.task("imagemin"));
+gulp.task("default", gulp.task("sass"));
+
+
+
+// 監視タスク
+gulp.task( 'watch', function() {
+  gulp.watch( 'scss/style.scss', gulp.task( 'sass' ) );
+});
+
---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+	const sample = document.querySelectorAll('.sample');
+	
+	// 画像が画面内に入ると、sample-animationクラスを追加する
+	// 画像が画面から出ると、sample-animationを削除する
+	const observer = new IntersectionObserver(function(entries) {
+		entries.forEach(function(entry) {
+		if (entry.intersectionRatio > 0) {
+			entry.target.classList.add('img-animation');
+		} else {
+			entry.target.classList.remove('img-animation');
+		}
+		});
+	});
+	
+	Array.prototype.forEach.call(sample, function(img) {
+		observer.observe(img);
+	});
+}
